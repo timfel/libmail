83dfcb08ec629b15e041337d1220cf5ac4d95740
Tim Felgentreff <timfelgentreff@gmail.com>
2012-08-22 12:49:29 +0200

    initial commit

---
+++ src/libmail.h
@@ -0,0 +1,29 @@
+/* 
+ * Copyright (C) 2012 by Tim Felgentreff
+ * 
+ * This file is part of libmail.
+ * 
+ * libmail is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.  libmail is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libmail.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBMAIL_H
+#define LIBMAIL_H
+
+#include "config.h"
+#include "libetpan.h"
+
+#if defined(__WIN32__) || defined(__MINGW32__)
+# define DLLImport __declspec(dllimport)
+#else
+# define DLLImport
+#endif
+
+#endif LIBMAIL_H
